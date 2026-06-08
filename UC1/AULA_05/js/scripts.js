761bee973a2ed801c9ab967daaf4c1b148ac16d1
Aluno <aluno@alunos.ce.senac.br>
2026-06-08 11:31:15 -0300

    AULA 05

---
+++ UC1/AULA_05/js/scripts.js
@@ -0,0 +1,24 @@
+//VARIÁVEL
+let nome = "Julio";
+nome = "daniele"; + " " + nome
+nome = "nicolas"; + " " + nome
+concatenar: juntar use +
+console.log("nome:" + nome)
+
+const nome = "Julio"
+nome = "Julio"
+
+const nomedev = "Julio"
+console.log("uso constante: " + nomedev)
+
+let nomeuser = "Julio"
+let idade = "28"
+let Estadocivil = "Solteiro"
+let naturalidade = "Fortaleza"
+let estado = "Ceará"
+
+let dados = "nome: "+nome+"\n idade: "+idade+"\n estado civil: "+Estadocivil+"\n naturalidade: "+naturalidade+"\n estado: "+estado
+// \n = quebra de linha
+
+
+console.log(dados)
