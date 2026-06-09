c02716a403aa5eccbae91d0689f3db2639a99a9d
Aluno <aluno@alunos.ce.senac.br>
2026-06-09 11:34:59 -0300

    condicionais_if

---
+++ UC1/AULA_06/CONDICIONAIS_IF/scripts.js
@@ -0,0 +1,28 @@
+let valor = 100
+
+if (valor===100) {
+    console.log(`o valor corresponde a ${valor}`)
+    } else {
+        console.log(`o valor ${valor} não corresponde a ${regra}`)
+
+    }
+
+    let idade = 18
+    if (idade >= 18) {
+        console.log(`com ${idade} anos, você é maior de idade`)
+    } else {
+        console.log(`
+            com ${idade} anos, você não é maior de idade`)
+    
+    }
+    
+   const faturamento = 32520.96
+   const metamensal = 45630.00
+if (faturamento>= metamensal)
+    console.log
+
+
+  }
+    
+
+
---
+++ UC1/AULA_06/assets/js/scripts.js
@@ -0,0 +1,53 @@
+// números e operadores
+let v1 = 10
+let v2 = 5
+
+const totalsoma = v1 + v2
+const totalsubtracao = v1 - v2
+const totalmultiplicacao = v1 * v2
+const totaldivisao = v1 / v2
+
+console.log("operações")
+console.log("\n valores: " + v1 + " e " + v2)
+
+resultado = "soma: " + totalsoma + "\n subtração: " + totalsubtracao + "\n multiplicação: " + totalmultiplicacao + "\n divisão: " + totaldivisao
+cosole.log(resultado)
+
+
+let num = 0
+num ++
+num ++ = 1
+num ++ = 2
+num ++ = 3
+num ++ = 4
+num ++ = 5
+
+
+let Produto = "Computador"
+let Valor = "2256"
+let Desconto = "750"
+const valorfinal = "valor - desconto"
+console.log("Produto: " + Produto + "\n Valor: " + Valor + "\n Desconto: " + Desconto + "\n Valor final: " + valorfinal)
+let 2256 = documentgetElementById("valor")
+let 750 = documentgetElementById("desconto")
+
+
+let somabr = totalsoma.toLocaleString("pt-br", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2
+});
+
+
+console.log(`
+    PRODUTOPROMOCIONAL
+PRODUTO: ${Produto}
+VALOR: R$ ${Valor}
+DESCONTO: R$ ${Desconto}
+VALOR FINAL: R$ ${valorfinal}
+`)
+    
+
+
+
+
+
